0162e2435f55610ed92f21e4f83b6cb60383a12a
BASIM-JAVED <basim.63297@iqra.edu.pk>
2024-03-13 00:37:21 +0500

    AdminLte integrate with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+// import Login from './component/Login';
+import Footer from './component/Footer';
+import Header from './component/Header';
+import Home from './component/Home';
+import SideNav from './component/SideNav';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+     {/* <Login></Login> */}
+
+<Header/>
+<Home/>
+<SideNav/>
+<Footer/>
+
     </div>
   );
 }
